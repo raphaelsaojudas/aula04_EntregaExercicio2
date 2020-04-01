a612092489f3ae4bf09b7530adedab7a1fccdee0
raphael gomes <37315248+raphaelsaojudas@users.noreply.github.com>
2020-03-31 23:09:55 -0300

    Add files via upload

---
+++ Aula04Ex2.java
@@ -0,0 +1,11 @@
+import javax.swing.JOptionPane;
+
+public class Aula04Ex2 {
+   public static void main (String arg[]){
+      for(int i=1;  i <= 100; i++){
+         if( i%2==0){
+            System.out.println("Par " + i);
+         }
+      }
+   }
+}
